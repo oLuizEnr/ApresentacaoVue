26703329b0fc2aff834b427aeb69c90aa176dd34
Luiz Enrique <luizenrique80025030@gmail.com>
2025-01-30 16:00:46 -0300

    Colocado o exemplo do Lucas (barra de pesquisa)

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const { createApp, ref } = Vue
 createApp({
   data() {
     return {
-      nome: 'Luiz',
-      idade: '16',
-      pesquisa: '',
-      eModoEscuro: false
+      eModoEscuro: false,
+      mensagem: "",
+      itens: [
+        "Item 1", "Item 2", "Item 3", "Teste", "Outro Item", "a", "b", "c", "d", "e", "f", "gustavo"
+      ]
     }
   },
   methods: {
@@ -15,6 +16,13 @@ createApp({
 
       const modoAtual = document.getElementById("modoAtual");
       modoAtual.href = this.eModoEscuro ? "modoEscuro.css" : "modoClaro.css";
+    }
+  },
+  computed: {
+    itensFiltrados() {
+      return this.itens.filter(
+        item => item.toLowerCase().includes(this.mensagem.toLowerCase())
+      );
     }
   }
 }).mount('#app')
